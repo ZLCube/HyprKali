b57b851584dbee71977bf7dc5c3efbf139ee2fcb
Cube <116672365+ZLCube@users.noreply.github.com>
2023-07-13 13:33:57 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,17 +4,20 @@ if [ "$(whoami)" == "root" ]; then
     exit 1
 fi
 
-ruta=$(pwd)
+path=$(pwd)
 
-#Creamos una carpeta llamada HyprSource en descargas
+#Create a path called HYprSource in Downloads folder
+#Creamos una carpeta llamada HyprSource en Descargas
 
 mkdir ~/Downloads/HyprSource
 
-#Copiamos la libreria a HyprSource
+#Copy libliftoff library to Hyprsource
+#Copiamos la libreria libliftoff a HyprSource
 
-cp $ruta/libliftoff-master.tar.gz ~/Downloads/HyprSource
+cp $path/libliftoff-master.tar.gz ~/Downloads/HyprSource
 
-#Instalamos las dependencias bprincipales de hyprland 
+#Install principal dependencies for Hyprland
+#Instalamos las dependencias principales de Hyprland 
 
 sudo apt-get install -y meson wget build-essential ninja-build cmake-extras cmake gettext gettext-base fontconfig libfontconfig-dev libffi-dev libxml2-dev libdrm-dev libxkbcommon-x11-dev libxkbregistry-dev libxkbcommon-dev libpixman-1-dev libudev-dev libseat-dev seatd libxcb-dri3-dev libvulkan-dev libvulkan-volk-dev  vulkan-validationlayers-dev libvkfft-dev libgulkan-dev libegl-dev libgles2 libegl1-mesa-dev glslang-tools libinput-bin libinput-dev libxcb-composite0-dev libavutil-dev libavcodec-dev libavformat-dev libxcb-ewmh2 libxcb-ewmh-dev libxcb-present-dev libxcb-icccm4-dev libxcb-render-util0-dev libxcb-res0-dev libxcb-xinput-dev xdg-desktop-portal-wlr hwdata libliftoff-dev libwlroots-dev
 
@@ -22,6 +25,7 @@ cd ~/Downloads && sudo apt-get install -y nala
 
 sudo nala install -y meson wget build-essential ninja-build cmake-extras cmake gettext gettext-base fontconfig libfontconfig-dev libffi-dev libxml2-dev libdrm-dev libxkbcommon-x11-dev libxkbregistry-dev libxkbcommon-dev libpixman-1-dev libudev-dev libseat-dev seatd libxcb-dri3-dev libvulkan-dev libvulkan-volk-dev  vulkan-validationlayers-dev libvkfft-dev libgulkan-dev libegl-dev libgles2 libegl1-mesa-dev glslang-tools libinput-bin libinput-dev libxcb-composite0-dev libavutil-dev libavcodec-dev libavformat-dev libxcb-ewmh2 libxcb-ewmh-dev libxcb-present-dev libxcb-icccm4-dev libxcb-render-util0-dev libxcb-res0-dev libxcb-xinput-dev libpango1.0-dev xdg-desktop-portal-wlr
 
+#More dependencies for Hyprland
 #Instalamos mas dependencias de hyprland
 
 cd ~/Downloads/HyprSource && wget https://github.com/hyprwm/Hyprland/releases/download/v0.24.1/source-v0.24.1.tar.gz && tar -xvf source-v0.24.1.tar.gz && rm -r source-v0.24.1.tar.gz
@@ -34,7 +38,8 @@ wget https://gitlab.freedesktop.org/emersion/libdisplay-info/-/releases/0.1.1/do
 
 tar -zxvf libliftoff-master.tar.gz && rm -r libliftoff-master.tar.gz
 
-#Compilamos los paquetes instalados 
+#Lets compile the tar packages
+#Compilamos los paquetes tar instalados 
 
 cd libliftoff-master && meson setup build/ && ninja -C build/ && cd ..
 
@@ -44,6 +49,7 @@ cd wayland-protocols-1.31 && mkdir build && cd build/ && meson setup --prefix=/u
 
 cd libdisplay-info-0.1.1 && mkdir build && cd build && meson setup --prefix=/usr --buildtype=release && ninja && sudo ninja install && cd ../..
 
+#Lets run HYprland
 #Ahora instalamos hyprland
 
 chmod a+rw hyprland-source/ && cd hyprland-source 
